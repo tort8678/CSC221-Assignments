c666cc52db3f9fa0b09d2aeb422588a4cd8e2ce1
tort8678 <102439231+tort8678@users.noreply.github.com>
2023-12-10 18:37:21 -0500

    Assignment 5 Creation

---
+++ Assignment5/app/src/main/java/com/example/assignment5/Question.java
@@ -0,0 +1,45 @@
+package com.example.assignment5;
+
+public class Question {
+    private int mTextResId;
+
+    private int mCorrectAnswer;
+
+    private int answers[];
+
+
+
+
+    public Question(int textResId, int correctAnswer, int a[]){
+        mTextResId = textResId;
+        mCorrectAnswer = correctAnswer;
+        answers = a;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public int getCorrectAnswer() {
+        return mCorrectAnswer;
+    }
+
+    public int getChoice(int index){
+        try{
+            return answers[index];
+        } catch(Exception e){
+            System.out.println(e);
+        }
+        return -1;
+    }
+
+
+
+    public void setTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public void setCorrectAnswer(int correctAnswer) {
+        mCorrectAnswer = correctAnswer;
+    }
+}
